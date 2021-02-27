4812aee93445654ca76af074ad029fc0d4fdc3eb
Marcos Noriega <marcosnoriega964@gmail.com>
2021-02-27 16:02:16 -0300

    tag module methods were completed

--- libs/inquireLib.js
+++ libs/inquireLib.js
@@ -1,6 +1,10 @@
 const { questions } = require('./questions');
 
 module.exports = ({inquirer}) => ({
+    /**
+     * execute the questions of CLI
+     * @returns {any}
+     */
     toAsk: async() => {
         const answers = await inquirer.prompt(questions)
 
--- libs/metaData.js
+++ libs/metaData.js
@@ -1,8 +1,26 @@
 module.exports = ({ NodeID3 }) => ({
-    writer: (tags) => {
+    /**
+     * it write tags in a audio file 
+     * @param {object} tags
+     * @param {string} filepath
+     * @returns {boolean}
+     */
+    writer: (tags, filepath) => {
         const success = NodeID3.write(tags, filepath);
+        // async
+        // NodeID3.write(tags, file, function(err) {  });
 
-        NodeID3.write(tags, file, function(err) {  })
+        return success;
+    },
+
+    /**
+     * it update tags in a audio file 
+     * @param {any} tags
+     * @param {string} filepath
+     * @returns {any}
+     */
+    update: (tags, filepath) => {
+        const success = NodeID3.update(tags, filepath);
 
         return success;
     }
